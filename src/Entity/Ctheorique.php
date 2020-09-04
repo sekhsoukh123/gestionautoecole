24137e3c2646c10dc7821b10be255913a367da66
sekhsoukh123 <70757497+sekhsoukh123@users.noreply.github.com>
2020-09-04 17:15:23 +0100

    Update Ctheorique.php

--- src/Entity/Ctheorique.php
+++ src/Entity/Ctheorique.php
@@ -1,44 +1,125 @@
 <?php
 
-namespace App\Form\Type;
+namespace App\Entity;
 
-use AppBundle\Form\DocumentHandlerType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use	Symfony\Component\Form\Extension\Core\Type\NumberType;
-class CtheoriqueType extends AbstractType
+use App\Repository\CtheoriqueRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity(repositoryClass=CtheoriqueRepository::class)
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Ctheorique
 {
     /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-
-            ->add('seance', NumberType::class,array(
-                'label' => 'ctheorique.label.seance'
-                ))
-
-
-            ;
-    }
+    private $id;
 
     /**
-     * @param OptionsResolver $resolver
+      * @var decimal
+      * @Assert\NotBlank(message="field.not_blank")
+      * @ORM\Column(name="seance", type="integer")
+      */
+     private $seance;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Candidat", inversedBy="ctheoriques", cascade={"persist"})
+     * @ORM\JoinColumn(name="candidat_id", referencedColumnName="id")
      */
-    public function configureOptions(OptionsResolver $resolver)
+    private $candidat;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="deleted", type="boolean")
+     */
+    private $deleted = 0;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated", type="datetime")
+     */
+    private $updated;
+
+
+
+    public function getId()
     {
-        $resolver->setDefaults(array(
-            'data_class' =>'App\Entity\Ctheorique',
-            'translation_domain' => 'Ctheorique',
-        ));
+        return $this->id;
+    }
+
+    public function getSeance()
+    {
+        return $this->seance;
+    }
+
+    public function setSeance($seance)
+    {
+        $this->seance = $seance;
+
+        return $this;
+    }
+
+
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    public function setDeleted(bool $deleted)
+    {
+        $this->deleted = $deleted;
+
+        return $this;
+    }
+
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    public function getCandidat()
+    {
+        return $this->candidat;
+    }
+
+    public function setCandidat($candidat)
+    {
+        $this->candidat = $candidat;
+
+        return $this;
     }
 }
